1e2c7400ac68f642f5fcf623bfd16e97a60ca614
doitkim <kimsh27214@gmail.com>
2022-05-03 09:27:02 +0900

    파이썬공부

---
+++ AI/DataAnalysis/DaegueAIschool/Package/stringLength.py
@@ -0,0 +1,8 @@
+def strlen(str):
+    return len(str)
+
+def strupper(str):
+    return str.upper()
+
+def strLower(str):
+    return str.lower()
---
+++ AI/DataAnalysis/DaegueAIschool/main.py
@@ -0,0 +1,8 @@
+# # import calcurator
+# from calcurator import plus, minus
+# print(plus(1,2))
+# print(minus(1,2))
+
+from Package import stringLength
+
+print(stringLength.strlen('string'))
